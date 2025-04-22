e87401336a71cec31bb1d4ead8b267c077891354
troy <troylu8@gmail.com>
2025-04-21 21:26:14 -0600

    edit app.sh, readme

--- README.md
+++ README.md
@@ -5,3 +5,18 @@
 [![website](https://img.shields.io/badge/-troylu.com-purple)](https://www.troylu.com)
 
 server for [nearsay](https://github.com/troylu8/nearsay)
+
+
+
+Most client communication happens with [socket.io (socketioxide)](https://crates.io/crates/socketioxide).
+
+Uses MongoDB for persistent storage and geospatial queries.
+
+Uses Redis as a cache but [I kinda regret it.]()
+
+<br>
+
+---
+<p align="center">
+    <img width="80" src="readme-resources/nearsay-icon.png">
+</o>
--- app.sh
+++ app.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+cd /home/ubuntu/nearsay/backend
+
 handle_exit() {
-    
+
     echo "shutting down mongod"
     mongod --dbpath db --shutdown
-    
+
     echo "shutting down redis instances"
     redis-cli -p 6000 shutdown
     redis-cli -p 6001 shutdown
-    
+
     echo "exiting!"
-    exit 0 
+    exit 0
 }
 trap handle_exit EXIT
 
@@ -22,5 +24,5 @@ redis-server --port 6000 --daemonize yes --save "" --appendonly no
 redis-server --port 6001 --daemonize yes --save "" --appendonly no
 
 echo "starting server"
-read -p "running"
-# ./nearsay-server.exe
+# read -p "running"
+./nearsay-server
